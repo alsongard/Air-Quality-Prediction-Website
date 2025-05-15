4197cb05a9ae4a73a0bb4eef9bab5e44762ed21b
alsongard <alsongard@gmail.com>
2025-05-15 14:54:17 +0300

    signup login page added

---
+++ air_quality_prediction/app/signup_login/page.tsx
@@ -0,0 +1,10 @@
+import FormLoginSignup from "../components/form_login_signup";
+
+export default function SignLoginPage()
+{
+    return (
+        <section className='w-full pt-[100px] h-[100vh] bg-linear-to-r from-[rgba(85,153,167,0.66)] to-[rgba(10,105,105,0.33)]'>
+            <FormLoginSignup/>
+        </section>
+    )
+}
